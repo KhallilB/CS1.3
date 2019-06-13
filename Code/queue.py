e4a05ef8e2759667661aca5d945d5902e23ccea6
KhallilB <khallil.bailey@gmail.com>
2019-06-12 21:30:46 -0700

    linked queue

--- Code/queue.py
+++ Code/queue.py
@@ -11,6 +11,7 @@ class LinkedQueue(object):
         """Initialize this queue and enqueue the given items, if any."""
         # Initialize a new linked list to store the items
         self.list = LinkedList()
+        self.size = 0
         if iterable is not None:
             for item in iterable:
                 self.enqueue(item)
@@ -21,31 +22,36 @@ class LinkedQueue(object):
 
     def is_empty(self):
         """Return True if this queue is empty, or False otherwise."""
-        # TODO: Check if empty
+        return self.list.is_empty()
 
     def length(self):
         """Return the number of items in this queue."""
-        # TODO: Count number of items
+        return self.list.length()
 
     def enqueue(self, item):
-        """Insert the given item at the back of this queue.
-        Running time: O(???) – Why? [TODO]"""
-        # TODO: Insert given item
+        """Insert the given item at the back of this queue."""
+        self.list.append(item)
 
     def front(self):
         """Return the item at the front of this queue without removing it,
         or None if this queue is empty."""
-        # TODO: Return front item, if any
+        if self.list.is_empty():
+            return None
+        return self.list.get_at_index(0)
 
     def dequeue(self):
         """Remove and return the item at the front of this queue,
-        or raise ValueError if this queue is empty.
-        Running time: O(???) – Why? [TODO]"""
-        # TODO: Remove and return front item, if any
-
+        or raise ValueError if this queue is empty."""
+        if self.is_empty():
+            raise ValueError('Queue is empty')
+        item = self.front()
+        self.list.delete(item)
+        return item
 
 # Implement ArrayQueue below, then change the assignment at the bottom
 # to use this Queue implementation to verify it passes all tests
+
+
 class ArrayQueue(object):
 
     def __init__(self, iterable=None):
@@ -69,8 +75,7 @@ class ArrayQueue(object):
         # TODO: Count number of items
 
     def enqueue(self, item):
-        """Insert the given item at the back of this queue.
-        Running time: O(???) – Why? [TODO]"""
+        """Insert the given item at the back of this queue."""
         # TODO: Insert given item
 
     def front(self):
@@ -80,8 +85,7 @@ class ArrayQueue(object):
 
     def dequeue(self):
         """Remove and return the item at the front of this queue,
-        or raise ValueError if this queue is empty.
-        Running time: O(???) – Why? [TODO]"""
+        or raise ValueError if this queue is empty."""
         # TODO: Remove and return front item, if any
 
 
