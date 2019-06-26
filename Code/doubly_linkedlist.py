ec21a08cb038af3a686d54ce62b2f13b20352c51
KhallilB <khallil.bailey@gmail.com>
2019-06-26 11:57:14 -0700

    added helper functions

--- Code/doubly_linkedlist.py
+++ Code/doubly_linkedlist.py
@@ -26,38 +26,16 @@ class DoublyLinkedList(object):
         """Return True if this linked list is empty, or False if not empty."""
         return self.head is None
 
-    def insert_empty_list(self, item):
-        """If the list is empty insert node at the start of linked list"""
-        # If the list is empty
-        if self.is_empty():
-            # Create new node
-            node = Node(item)
-            # Make the new node the head node
-            self.head = node
-            # Increase the size
-            self.size += 1
-        else:
-            raise ValueError('List is Empty')
+    def insert_before(self, ref, node):
+        """Inserts a node after a given reference"""
+
+    def insert_after(self, ref, node):
+        """Inserts a node before a given reference"""
+        pass
 
     def append(self, item):
         """Insert the given item at the beginning of the list"""
-        # If the head is empty:
-        if self.is_empty:
-            # Create new node
-            node = Node(item)
-            # Created node becomes new head node
-            self.head = node
-            # Exit
-            return
-        # Otherwise if its not empty
-        # Create new node
-        node = Node(item)
-        # Point the head to the next node
-        node.next = self.head
-        # Point the node back toward the head
-        self.head.previous = node
-        # Head becomes the new node
-        self.head = node
+        pass
 
     def prepend(self, item):
         """ Insert the given item at the end of the list"""
